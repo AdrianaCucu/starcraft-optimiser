a7f278f779f6cc65a6f322cde8de0fd4d0671c95
Adriana Cucu <adrianacucu99@gmail.com>
2019-03-04 15:08:44 +0000

    Created building classes.

---
+++ src/buildings/SupplyDepot.java
@@ -0,0 +1,17 @@
+package buildings;
+
+public class SupplyDepot extends Building {
+
+    /**
+     * Constructor for Supply Depot.
+     *
+     * Mineral cost: 100
+     * Gas cost: 0
+     * Build Time: 30 seconds
+     * Dependency: none
+     */
+    public SupplyDepot() {
+        super(100, 0, 30, null);
+    }
+
+}

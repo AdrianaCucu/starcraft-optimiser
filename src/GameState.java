44503e2280090dd07b029c77f695f9edf997580a
amlo <amlo@pc3-059-l.cs.st-andrews.ac.uk>
2019-03-08 17:14:03 +0000

    made changes to game state

--- src/GameState.java
+++ src/GameState.java
@@ -4,6 +4,7 @@ import buildings.*;
 import resources.*;
 
 public class GameState {
+    private GameState gs;
     private int time;
 
     private List<Barracks> barracks;
@@ -23,14 +24,8 @@ public class GameState {
 
 
 
-
-
     public GameState() {
-
-    }
-
-
-    public void initialiseGame() {
+        this.time = 0;
         this.barracks = new ArrayList<>();
         this.commandCenters = new ArrayList<>();
         this.factories = new ArrayList<>();
@@ -47,12 +42,102 @@ public class GameState {
         this.patchWorkers = new ArrayList<>();
     }
 
-    public static void updateState(Unit unit) {
+
+    /**
+     * Initialises game and creates first starting game state.
+     * @return
+     */
+    public GameState initialiseGame() {
+        GameState newGs;
+        gs = new GameState();
+
+        gs.updateState(new CommandCenter());
+
+        gs.updateState(new Worker());
+        gs.updateState(new Worker());
+        gs.updateState(new Worker());
+        gs.updateState(new Worker());
+        gs.updateState(new Worker());
+        gs.updateState(new Worker());
+
+        Minerals.total = 50;
+        Gas.total = 0;
+
+        Timer.setSecondsElapsed(0);
+
+        newGs = gs;
+
+        return newGs;
 
     }
 
-    public static void updateState(Building building) {
+
+    /**
+     * Updates state of game accordingly.
+     * @param unit
+     */
+    public void updateState(Unit unit) {
+        String unitType = unit.IDENT;
+
+        switch (unitType) {
+            case Hellion.IDENT: {
+                hellions.add(unit);
+                break;
+            }
+            case Marine.IDENT: {
+                marines.add(unit);
+                break;
+            }
+            case Medivac.IDENT: {
+                medivacs.add(unit);
+                break;
+            }
+            case Viking.IDENT: {
+                vikings.add(unit);
+                break;
+            }
+            case Worker.IDENT: {
+                unassignedWorkers.add(unit);
+                break;
+            }
+            default: {
+
+            }
+
+        }
 
     }
+
+    public void updateState(Building building) {
+        String buildingType = building.IDENT;
+
+        switch (buildingType) {
+            case Barracks.IDENT: {
+                barracks.add(building);
+                break;
+            }
+            case CommandCenter.IDENT: {
+                commandCenters.add(building);
+                break;
+            }
+            case Factory.IDENT: {
+                factories.add(building);
+                break;
+            }
+            case Refinery.IDENT: {
+                refineries.add(building);
+                break;
+            }
+            case Starport.IDENT: {
+                starports.add(building);
+                break;
+            }
+            default: {
+
+            }
+
+        }
+    }
+
 
 }
--- src/GameStates.java
+++ src/GameStates.java
@@ -2,5 +2,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class GameStates {
-    List<GameState> gs = new ArrayList<>();
+    private static List<GameState> gs = new ArrayList<>();
+
+    public void addNewState() {
+        gs.add();
+    }
+
 }

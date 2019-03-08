b06f04a70d8bd1a421d931fa0d07ce2dfc02aec8
Adriana Cucu <adrianacucu99@gmail.com>
2019-03-08 15:35:36 +0000

    Added base.

--- src/resources/Gas.java
+++ src/resources/Gas.java
@@ -2,5 +2,4 @@ package resources;
 
 public class Gas extends Resource {
 
-    private int geysers = 2;
 }
--- src/resources/Minerals.java
+++ src/resources/Minerals.java
@@ -2,5 +2,4 @@ package resources;
 
 public class Minerals extends Resource {
 
-    private int patches = 8;
 }
